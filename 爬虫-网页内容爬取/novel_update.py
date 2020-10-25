a1ab46f9584b2bd023d92eccf9019911037a1044
zzs01 <36794188+zzs01@users.noreply.github.com>
2020-10-25 21:31:02 +0800

    使用数据库

--- 爬虫-网页内容爬取/novel_update.py
+++ 爬虫-网页内容爬取/novel_update.py
@@ -5,7 +5,53 @@ import re
 import sys
 from time import strftime   
 from datetime import datetime
-from pypinyin import lazy_pinyin
+import sqlite3
+
+
+# 查询数据    
+def query_sql(name,query_key):
+    conn = sqlite3.connect('./novel.db')
+    cur = conn.cursor()
+    que_sql = '''SELECT %s from NOVEL where NAME = "%s"''' % (query_key,name)
+    
+    tb_sql = '''CREATE TABLE NOVEL 
+               (NAME   CHAR(50)  PRIMARY KEY  UNIQUE  NOT NULL,
+               LINK             CHAR(50),
+               LOCATION         INT,
+               CHAPTER          CHAR(50));'''
+    de_sql = '''DROP TABLE NOVEL;'''
+    try:
+        cursor = cur.execute(que_sql)
+    except Exception as er:
+        try:
+            cur.execute(tb_sql)
+        except Exception as err:
+            cur.execute(de_sql)
+            cur.execute(tb_sql)
+        cursor = cur.execute(que_sql)
+    data_tupple_list = cursor.fetchall()
+    data_list = [str(i[0]) for i in data_tupple_list if i[0] != '']
+    data = "".join(data_list)
+    conn.close()
+    return data
+
+# 添加或更新数据库    
+def create_sql(novel_name, link, location=-1, chapter=""):
+    conn = sqlite3.connect('./novel.db')
+    cur = conn.cursor()
+    ins_sql = '''INSERT INTO NOVEL (NAME,LINK,LOCATION,CHAPTER) \
+        VALUES ("%s", "%s", %s, "%s") \
+        ''' % (novel_name, link, location, chapter)
+    up_sql = '''UPDATE NOVEL set LINK = "%s",LOCATION = %s,CHAPTER = "%s"  where NAME = "%s"''' % (link, location, chapter, novel_name)
+    
+    if query_sql(novel_name,"name"):
+        cur.execute(up_sql)
+    else:
+        cur.execute(ins_sql)
+        
+    conn.commit()
+    conn.close()
+
 #爬取并推送小说最新章节
 #来源，笔趣阁
 #首次运行只发送最新一章
@@ -14,14 +60,12 @@ headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,imag
 'Accept-Language': 'zh-CN,zh;q=0.9',
 'Cache-Control': 'no-cache',
 'Connection': 'keep-alive',
-'Cookie': 'clickbids=1734%2C75601; Hm_lvt_6dfe3c8f195b43b8e667a2a2e5936122=1602565905,1603439575,1603510942; Hm_lpvt_6dfe3c8f195b43b8e667a2a2e5936122=1603510942',
-'Host': 'www.biquge.info',
 'Pragma': 'no-cache',
 'Upgrade-Insecure-Requests': '1',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.116 Safari/537.36}'
 }
 # 获取小说链接
-def get_novel_link(searchkey):
+def get_novel_link(novel_name, searchkey):
     home_page = "http://www.biquge.info/"
 #第二个源    https://www.shujy.com/ 章节名字不一样
     html = requests.get(home_page,headers=headers)
@@ -39,6 +83,7 @@ def get_novel_link(searchkey):
         dd_send("找不到书籍信息", "书籍不存在", "请检查书名" + searchkey + "是否正确")
         return ""
     novel_link = home_page+red
+    create_sql(novel_name, novel_link)
     return novel_link
     
 # 钉钉推送
@@ -70,13 +115,19 @@ def get_novel_content(chapter_link, chapter_title, novel_link, novel_name):
     re_novel_content = r'<div id="content"><!--go-->(.*?)</div>'
     chapter_content = re.findall(re_novel_content,content,re.DOTALL)[0]
     chapter_content = chapter_content.replace(r"<br/>",'\n')
-    dd_send(novel_name, chapter_title,chapter_content)
+    drop_content = "正在手打中，请稍等片刻，内容更新后"
+    if not re.search(drop_content,chapter_content):
+        dd_send(novel_name, chapter_title,chapter_content)
+    else:
+        return "failed"
     
 # 获取小说更新状态    
 def get_novel_status(novel_name, newest_chapter_location):    
     search_key = novel_name
-    novel_name_en = ("").join(lazy_pinyin(search_key))
-    novel_link = get_novel_link(search_key)
+    
+    novel_link = query_sql(novel_name,"link")
+    if not novel_link:
+        novel_link = get_novel_link(novel_name, search_key)
     
     # 小说不存在
     if not novel_link:
@@ -105,24 +156,25 @@ def get_novel_status(novel_name, newest_chapter_location):
             return this_newest
         while saved_content != novel_title[last_newest]:
             last_newest = last_newest -1
+            if newest - last_newest > 10:
+                get_novel_link(novel_name,search_key)
+                get_novel_status(novel_name, newest_chapter_location)
+                return
         else:
             return last_newest + 1
-            
+
     #查询是否最新一章
-    try:
-        with open( local_path + "/" + novel_name_en + ".txt", 'r', encoding='utf-8') as fh:
-            content = fh.read()
-    except Exception as er:
-        content = ""        
+    content = query_sql(novel_name,"chapter")
         
     if content == latest_chapter_title:
         return
     else:
         for i in range(update_chapters(content, newest - 1), newest + 1):
-            get_novel_content(novel_content[i],novel_title[i], novel_link, search_key)
-            
-        with open( local_path + "/" + novel_name_en + ".txt", 'w', encoding='utf-8') as fs:
-            fs.write(latest_chapter_title)
+            if_update = get_novel_content(novel_content[i],novel_title[i], novel_link, search_key)
+            if if_update == "failed":
+                create_sql(novel_name, novel_link, newest_chapter_location, novel_title[i])
+                return
+        create_sql(novel_name, novel_link, newest_chapter_location, latest_chapter_title)
     
     #给另一个文件传参
     global status
