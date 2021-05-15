84c740b603f408dad5b45b30f475484049de2b90
张祖士 <1227997250@qq.com>
2021-05-15 16:38:17 +0800

    update E001-彩票/001-彩票数据爬取.py.

--- E001-彩票/001-彩票数据爬取.py
+++ E001-彩票/001-彩票数据爬取.py
@@ -90,11 +90,7 @@ def lottery_database_create(history_lottery_url,table_name):
     conn.commit()
     conn.close()
 
-def data_visualization(table_name):
-    """
-    数据可视化
-    :return:
-    """
+def data_read(table_name):
     # 准备数据
     conn = sqlite3.connect('lottery.db')
     form = conn.cursor()
@@ -103,45 +99,112 @@ def data_visualization(table_name):
     lo_data_dic = {}
     for row in cursor:
         lo_data_dic.update({row[0]:row[1]})
-        # print('lotteryDrawNum = ',row[0])
-        # print('lotteryDrawResult = ', row[1],'\n')
+
     # 按照key排序
     lo_data_dic = sorted(lo_data_dic.items(), key=lambda d: d[0])
     lotteryDrawNum = sorted([row[0] for row in lo_data_dic])
-    # print(lotteryDrawNum)
-    lotteryDrawResult = [int(row[1][0]) for row in lo_data_dic]
-    # print(lotteryDrawResult)
+    lotteryDrawResult = [int(row[1][2]) for row in lo_data_dic]
     conn.close()
+    return lotteryDrawNum, lotteryDrawResult
 
+def data_visualization_plt(table_name1,table_name2):
+    """
+    数据可视化-折线图
+    :return:
+    """
     # 创建画布
-    plt.figure()
+    plt.figure(figsize=(15,8),dpi=100)
 
+    y_list1 = data_read(table_name1)[1]
+    y_list2 = data_read(table_name2)[1]
+    x_list1 = [i+1 for i in range(len(y_list1))]
+    x_list2 = [i+1 for i in range(len(y_list2))]
     # 绘画折线图
-    plt.plot(lotteryDrawNum,  # x轴数据
-             lotteryDrawResult,  # y轴数据
+    plt.plot(x_list1,  # x轴数据
+             y_list1,  # y轴数据
              linestyle='-',  # 折线类型
              linewidth=2,  # 折线宽度
              color='steelblue',  # 折线颜色
              marker='o',  # 折线图中添加圆点
-             markersize=6,  # 点的大小
-             markeredgecolor='black',  # 点的边框色
-             markerfacecolor='brown')  # 点的填充色
-    # 获取图的坐标信息
-    # ax = plt.gca()
-    # 设置日期的显示格式
-    # date_format = mpl.dates.DateFormatter("%m-%d")
-    # ax.xaxis.set_major_formatter(date_format)
-    # # 设置x轴每个刻度的间隔天数
-    # xlocator = mpl.ticker.MultipleLocator(7)
-    # ax.xaxis.set_major_locator(xlocator)
-    # 添加x，y轴标签以及图表名称
-    plt.ylabel('开奖结果', fontsize=18)  # 横坐标轴的标题
-    plt.xlabel('期号', fontsize=18)  # 纵坐标轴的标题
+             markersize=3,  # 点的大小
+             # markeredgecolor='black',  # 点的边框色
+             # markerfacecolor='brown', # 点的填充色
+             label= table_name1)
+
+    plt.plot(x_list2,  # x轴数据
+             y_list2,  # y轴数据
+             linestyle='-',  # 折线类型
+             linewidth=2,  # 折线宽度
+             color='red',  # 折线颜色
+             marker='o',  # 折线图中添加圆点
+             markersize=3,  # 点的大小
+             # markeredgecolor='black',  # 点的边框色
+             # markerfacecolor='brown', # 点的填充色
+             label= table_name2)
+
+    # 添加图例
+    plt.legend()
+
+    # 添加x，y轴刻度
+    yticks = range(0,10,1)
+    plt.yticks(yticks)
+
+    # 设置x，y轴名称以及标题
+    plt.ylabel('开奖结果', fontsize=18)
+    plt.xlabel('期号', fontsize=18)
     plt.title('七星彩图表', fontsize=18)
+    # 显示网格
+    plt.grid(True, linestyle = '--', alpha = 0.5)
+    plt.show()
 
-    plt.grid() # 显示网格
-    # 添加图形标题
+def data_visualization_bar(table_name1,table_name2):
+    """
+    数据可视化-柱状图
+    :return:
+    """
+    # 创建画布
+    plt.figure(figsize=(15, 8), dpi=100)
 
+    x_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y_list1 = data_read(table_name1)[1]
+    y_sum1 = len(y_list1)
+    print('%s的平均数为：%s'%(table_name1,sum(y_list1)/y_sum1))
+    # print(y_sum1)
+    y_list1 = [(y_list1.count(i))/y_sum1 for i in x_list]
+
+    y_list2 = data_read(table_name2)[1]
+    y_sum2 = len(y_list2)
+    print('%s的平均数为：%s' % (table_name2, sum(y_list2) / y_sum2))
+    # print(y_sum2)
+    y_list2 = [(y_list2.count(i))/y_sum2 for i in x_list]
+
+    total_width, n = 0.6, 2
+    width = total_width / n
+
+    x_list1 = [x_list[i] - width/2 for i in x_list]
+    plt.bar(x_list1, y_list1, width=width, color="green", label = table_name1)
+    for a, b in zip(x_list, y_list1):  # 柱子上的数字显示
+        plt.text(a-width/2, b, '%.3f' % b, ha='center', va='bottom', fontsize=10)
+    # for i in range(len(x_list)):
+    #     x_list[i] = x_list[i] + width/2
+    x_list2 = [x_list[i] + width / 2 for i in x_list]
+    plt.bar(x_list2, y_list2, width=width, color="blue", label =table_name2)
+    for a, b in zip(x_list, y_list2):  # 柱子上的数字显示
+        plt.text(a+width/2, b, '%.3f' % b, ha='center', va='bottom', fontsize=10)
+
+    # 添加图例
+    plt.legend(fontsize = 10)
+
+    # 添加x，y轴刻度
+    xticks = range(0,10,1)
+    plt.xticks(xticks)
+
+    # 设置x，y轴名称以及标题
+    plt.ylabel('开奖结果', fontsize=18)
+    plt.xlabel('期号', fontsize=18)
+    plt.title('七星彩图表', fontsize=18)
+    # 显示网格
+    plt.grid(True, linestyle = '--', alpha = 0.5)
     plt.show()
 
 def main():
@@ -151,13 +214,16 @@ def main():
     #     print('网络有误，未更新数据...')
     #     pass
     # #
-    history_lottery_url = "https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=04&provinceId=0&pageSize=30&isVerify=1&pageNo={}&startTerm=20001&endTerm=20133"
-    for i in range(1,6):
-        print('第 %d 页数据写入。。。'%i)
-        lottery_database_create(history_lottery_url.format(i),'LOTTERY2020')
-        print(history_lottery_url.format(i))
-
-    data_visualization('LOTTERY2020')
+    # history_lottery_url = "https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=04&provinceId=0&pageSize=30&isVerify=1&pageNo={}&startTerm=20001&endTerm=20133"
+    # for i in range(1,7):
+    #     print('第 %d 页数据写入。。。'%i)
+    #     lottery_database_create(history_lottery_url.format(i),'LOTTERY2020')
+    #     print(history_lottery_url.format(i))
+    #
+    table_name1 = 'LOTTERY2019'
+    table_name2 = 'LOTTERY2020'
+    data_visualization_bar(table_name1,table_name2)
+    # data_visualization_plt(table_name1,table_name2)
 
 if __name__ == '__main__':
     main()
